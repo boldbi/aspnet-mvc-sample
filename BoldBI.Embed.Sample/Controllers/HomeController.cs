8add48718a35eeb8fb5fc9f0218975846ac95623
Nithya <nithya.gopal@syncfusion.com>
2023-05-30 07:54:26 +0530

    313758-Configured-the-EmbedProperties-by-EmbedConfig-Json

--- BoldBI.Embed.Sample/Controllers/HomeController.cs
+++ BoldBI.Embed.Sample/Controllers/HomeController.cs
@@ -43,10 +43,10 @@ namespace BoldBI.Embed.Sample.Controllers
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri(EmbedProperties.RootUrl);
+                client.BaseAddress = new Uri(GlobalAppSettings.EmbedDetails.ServerUrl);
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Add("Authorization", token.TokenType + " " + token.AccessToken);
-                var result = client.GetAsync(EmbedProperties.RootUrl + "/api/" + EmbedProperties.SiteIdentifier + "/v2.0/items?ItemType=2").Result;
+                var result = client.GetAsync(GlobalAppSettings.EmbedDetails.ServerUrl + "/api/" + GlobalAppSettings.EmbedDetails.SiteIdentifier + "/v2.0/items?ItemType=2").Result;
                 string resultContent = result.Content.ReadAsStringAsync().Result;
                 return resultContent;
             }
@@ -57,16 +57,16 @@ namespace BoldBI.Embed.Sample.Controllers
             using (var client = new HttpClient())
             {
                 System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12;
-                client.BaseAddress = new Uri(EmbedProperties.RootUrl);
+                client.BaseAddress = new Uri(GlobalAppSettings.EmbedDetails.ServerUrl);
                 client.DefaultRequestHeaders.Accept.Clear();
 
                 var content = new FormUrlEncodedContent(new[]
                 {
-                    new KeyValuePair<string, string>("Username", EmbedProperties.UserEmail),
+                    new KeyValuePair<string, string>("Username", GlobalAppSettings.EmbedDetails.UserEmail),
                     new KeyValuePair<string, string>("grant_type", "embed_secret"),
-                    new KeyValuePair<string, string>("embed_secret", EmbedProperties.EmbedSecret)
+                    new KeyValuePair<string, string>("embed_secret", GlobalAppSettings.EmbedDetails.EmbedSecret)
                 });
-                var result = client.PostAsync(EmbedProperties.RootUrl + "/api/" + EmbedProperties.SiteIdentifier + "/token", content).Result;
+                var result = client.PostAsync(GlobalAppSettings.EmbedDetails.ServerUrl + "/api/" + GlobalAppSettings.EmbedDetails.SiteIdentifier + "/token", content).Result;
                 string resultContent = result.Content.ReadAsStringAsync().Result;
                 var response = JsonConvert.DeserializeObject<Token>(resultContent);
                 return response;
@@ -77,7 +77,7 @@ namespace BoldBI.Embed.Sample.Controllers
         [Route("embeddetail/get")]
         public ActionResult GetEmbedDetails(string embedQuerString, string dashboardServerApiUrl)
         {
-            embedQuerString += "&embed_user_email=" + EmbedProperties.UserEmail;
+            embedQuerString += "&embed_user_email=" + GlobalAppSettings.EmbedDetails.UserEmail;
             var embedDetailsUrl = "/embed/authorize?" + embedQuerString.ToLower() + "&embed_signature=" + GetSignatureUrl(embedQuerString.ToLower());
 
             using (var client = new HttpClient())
@@ -95,7 +95,7 @@ namespace BoldBI.Embed.Sample.Controllers
         public string GetSignatureUrl(string message)
         {
             var encoding = new System.Text.UTF8Encoding();
-            var keyBytes = encoding.GetBytes(EmbedProperties.EmbedSecret);
+            var keyBytes = encoding.GetBytes(GlobalAppSettings.EmbedDetails.EmbedSecret);
             var messageBytes = encoding.GetBytes(message);
             using (var hmacsha1 = new HMACSHA256(keyBytes))
             {
