5e6bc24280796d3fbd34e4643cb64df06c5418b8
Nithya <nithya.gopal@syncfusion.com>
2023-05-30 13:35:57 +0530

    313758-Configured-the-EmbedProperties-by-EmbedConfig-Json

--- BoldBI.Embed.Sample/Controllers/HomeController.cs
+++ BoldBI.Embed.Sample/Controllers/HomeController.cs
@@ -74,8 +74,8 @@ namespace BoldBI.Embed.Sample.Controllers
         }
 
         [HttpPost]
-        [Route("embeddetail/get")]
-        public ActionResult GetEmbedDetails(string embedQuerString, string dashboardServerApiUrl)
+        [Route("AuthorizationServer")]
+        public ActionResult AuthorizationServer(string embedQuerString, string dashboardServerApiUrl)
         {
             embedQuerString += "&embed_user_email=" + GlobalAppSettings.EmbedDetails.UserEmail;
             var embedDetailsUrl = "/embed/authorize?" + embedQuerString.ToLower() + "&embed_signature=" + GetSignatureUrl(embedQuerString.ToLower());
