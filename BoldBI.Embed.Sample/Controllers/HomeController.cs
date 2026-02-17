80f4446090f204181791a31dbda5737496f1c228
anuabarna.bsf19 <anuabarna.b@syncfusion.com>
2026-02-17 16:04:07 +0530

    updated sample with embedtoken

--- BoldBI.Embed.Sample/Controllers/HomeController.cs
+++ BoldBI.Embed.Sample/Controllers/HomeController.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Net.Http;
-using System.Web.Mvc;
+﻿using BoldBI.Embed.Sample.Models;
 using Newtonsoft.Json;
-using BoldBI.Embed.Sample.Models;
-using System.Security.Cryptography;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Web.Mvc;
 
 namespace BoldBI.Embed.Sample.Controllers
 {
@@ -78,33 +76,32 @@ namespace BoldBI.Embed.Sample.Controllers
         }
 
         [HttpPost]
-        [Route("AuthorizationServer")]
-        public ActionResult AuthorizationServer(string embedQuerString, string dashboardServerApiUrl)
+        [Route("TokenGeneration")]
+        public string TokenGeneration()
         {
-            embedQuerString += "&embed_user_email=" + GlobalAppSettings.EmbedDetails.UserEmail;
-            var embedDetailsUrl = "/embed/authorize?" + embedQuerString.ToLower() + "&embed_signature=" + GetSignatureUrl(embedQuerString.ToLower());
-
-            using (var client = new HttpClient())
+            var embedDetails = new
             {
-                client.BaseAddress = new Uri(dashboardServerApiUrl);
-                client.DefaultRequestHeaders.Accept.Clear();
+                email = GlobalAppSettings.EmbedDetails.UserEmail,
+                serverurl = GlobalAppSettings.EmbedDetails.ServerUrl,
+                siteidentifier = GlobalAppSettings.EmbedDetails.SiteIdentifier,
+                embedsecret = GlobalAppSettings.EmbedDetails.EmbedSecret,
+                dashboard = new  // Dashboard ID property is mandatory only when using BoldBI version 14.1.11.
+                {
+                    id = GlobalAppSettings.EmbedDetails.DashboardId
+                }
+            };
 
-                var result = client.GetAsync(dashboardServerApiUrl + embedDetailsUrl).Result;
-                string resultContent = result.Content.ReadAsStringAsync().Result;
-                return Json(resultContent);
-            }
-        }
-        
-        public string GetSignatureUrl(string message)
-        {
-            var encoding = new System.Text.UTF8Encoding();
-            var keyBytes = encoding.GetBytes(GlobalAppSettings.EmbedDetails.EmbedSecret);
-            var messageBytes = encoding.GetBytes(message);
-            using (var hmacsha1 = new HMACSHA256(keyBytes))
-            {
-                var hashMessage = hmacsha1.ComputeHash(messageBytes);
-                return Convert.ToBase64String(hashMessage);
-            }
+            //Post call to Bold BI server
+            var client = new HttpClient();
+            var requestUrl = $"{embedDetails.serverurl}/api/{embedDetails.siteidentifier}/embed/authorize";
+
+            var jsonPayload = JsonConvert.SerializeObject(embedDetails);
+            var httpContent = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+            var result = client.PostAsync(requestUrl, httpContent).Result;
+            var resultContent = result.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<dynamic>(resultContent).Data.access_token;
         }
     }
 }
